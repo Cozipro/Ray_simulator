c43c39d4881ba98551c7e1910fd2cbd521525f74
Cozipro <64368088+Cozipro@users.noreply.github.com>
2021-04-22 23:24:55 +0200

    Création fichier Ray_Simulator

--- Ray_simulator/Ray_simulator.py
+++ Ray_simulator/Ray_simulator.py
@@ -51,9 +51,13 @@ class rayon:
             Y1 = (X1-self.x)*np.tan(self.teta) +self.y  #Calcul de l'ordonnée du point de contact
 
             if round(X1) == round(self.x): #Sécurité pour éviter de créer un deuxième rayon réfléchi au point de départ d'un rayon réfléchi
-                
+                print('continue')
                 continue
-            
+            print("y1 < miroir.max", Y1 <= miroir.max, Y1)
+            print("y1> miroir.min", Y1 >= miroir.min, miroir.max)
+            print((((self.direction == False) and (self.x > miroir.x)) or ((self.direction == True) and (self.x < miroir.x))))
+            print(X1 >= round(np.min(miroir.xc)) and X1 <= round(np.max(miroir.xc)))
+            print("x_array", X1 <= max(self.x_array))
             #On vérifie si le programme n'as pas choisi la mauvaise solution, et que la solution est bien sur le miroir
             if Y1 < miroir.max and Y1 > miroir.min and (((self.direction == False) and (self.x > miroir.x)) or ((self.direction == True) and (self.x < miroir.x)))  and round(X1,1) >= round(np.min(miroir.xc),1) and round(X1,1) <= round(np.max(miroir.xc),1) and X1 <= max(self.x_array):
                 
@@ -62,12 +66,14 @@ class rayon:
                 teta_rayon = np.arcsin(Y1/miroir.r)        #On calcule l'angle de la normale
                 teta_nouveau = -np.pi + 2*teta_rayon -self.teta    #On calcule l'angle du rayon réfléchi
 
-
+                print("teta_rayon", teta_rayon)
+                print("teta_nouveau", teta_nouveau)
+                print("teta", self.teta)
                 if abs(teta_nouveau) > np.pi/2 and abs(teta_nouveau) < 3*np.pi/2: #On définit la direction du rayon en fonction de son angle
                     direction = False
                 else:
                     direction = True
-
+                print("direction:",direction)
                 #On créé un nouveau rayon (réfléchi) en fonction du point de contact avec le miroir, l'angle et sa direction
                 lst_ray.append(rayon((self.fig,self.ax),X1,Y1, teta_nouveau, origine = miroir, direction = direction))
 
@@ -134,67 +140,24 @@ if __name__ == "__main__":
     lst_miroir = []
     lst_source = []
     
-    def trace(ouverture, diametre, rayon, inf):
-        #Limites, grille, ratio des axes..
-        fig[1].set_xlim(-10,10)
-        fig[1].set_ylim(-7,7)
-        fig[1].grid(True)
-        fig[1].set_aspect("equal")
-
-        #On créé les objets miroir et source que l'on ajoute dans la liste correspondant
-        lst_miroir.append(miroir(position = 7, r=rayon, dia = diametre, figure = fig, color = "blue")) 
-        lst_source.append(source(fig,-10, 0,ouverture, 8, inf = inf, height = 8))
     
-    #Création des wigets
-    axe_teta = plt.axes([0.1, 0.92, 0.2, 0.03]) #Left, bottom, width, height
-    axe_dia = plt.axes([0.4, 0.92, 0.2, 0.03]) 
-    axe_rayon = plt.axes([0.7,0.92,0.2,0.03])
-    axe_infiny = plt.axes([0.025, 0.7, 0.1, 0.1])
-    axe_r = plt.axes([0.025, 0.5, 0.1, 0.1])
+    #Limites, grille, ratio des axes..
+    fig[1].set_xlim(-20,20)
+    fig[1].set_ylim(-15,15)
+    fig[1].grid(True)
+    fig[1].set_aspect("equal")
 
-    slider_teta = wdg.Slider(axe_teta, 'Ouverture', 0, np.pi/4, valinit=np.pi/6)
-    slider_diametre = wdg.Slider(axe_dia, 'Diamètre', 0, np.pi/2, valinit=np.pi/6)
-    slider_rayon = wdg.Slider(axe_rayon, "Rayon", 0.1, 15, valinit=10)
-    button_inf = wdg.RadioButtons(axe_infiny, ('Infinie', 'Non'))
-    button_type = wdg.RadioButtons(axe_r, ("Concave", "Convexe"))
-
+    #On créé les objets miroir et source que l'on ajoute dans la liste correspondant
+    lst_miroir.append(miroir(position = -10, r=-15, dia = np.pi/4, figure = fig, color = "blue")) 
+    lst_miroir.append(miroir(position = 7, r=-10, dia = np.pi/4, figure = fig, color = "blue")) 
+    lst_source.append(source(fig,-20, 0,np.pi/4, 4, inf = True, height = 8))
+    
 
 
-    def mise_a_jour(val=None):
 
-        #On enlève tous les rayon/miroirs/sources des listes pour en créer des nouveaux
-        for rayon in lst_ray:
-            lst_ray.remove(rayon)
-        for sourcee in lst_source:
-            lst_source.remove(sourcee)
-        for miroire in lst_miroir:
-            lst_miroir.remove(miroire)
-        fig[1].cla()    #Clear de la figure
-        
-        #On récupère la valeur des widgets
-        ouverture = slider_teta.val
-        diametre = slider_diametre.val
-        rayon = slider_rayon.val
 
-        #Si infini est choisi, la variable "infiny" devient True, et inversement
-        if button_inf.value_selected == "Infinie":
-            infiny = True
-        else:
-            infiny = False
-        
-        #Si convexe est choisi, le rayon est inversé.
-        if button_type.value_selected == "Convexe":
-            rayon *= -1
 
-        trace(ouverture = ouverture, diametre = diametre, rayon = rayon, inf = infiny)
-        
-
-    #Définition de la fonction à appeler lorsque le widget a été touché
-    slider_teta.on_changed(mise_a_jour)
-    slider_diametre.on_changed(mise_a_jour)
-    slider_rayon.on_changed(mise_a_jour)
-    button_inf.on_clicked(mise_a_jour)
-    button_type.on_clicked(mise_a_jour)
-
-    mise_a_jour()   #On appelle une premiere fois la fonction mise_a_jour() pour réaliser le premier plot
     plt.show()
+
+
+
